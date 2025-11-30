258a6ba6174e3f453a80ad987e8e0cdf995dfcf7
Ojas Sadgir <ojas.sadgir@ust.com>
2025-11-30 15:18:30 +0530

    Adding updated code

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,24 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("trade-app-v1").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "manifest.json",
-        "icon-192.png",
-        "icon-512.png"
-      ]);
+const CACHE_NAME = "trade-check-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
